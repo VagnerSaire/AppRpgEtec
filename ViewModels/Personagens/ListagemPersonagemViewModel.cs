d58774c345048c3baee267a99e8106023b5d35b8
etechas <etechas@LI02-10>
2025-08-26 22:34:44 -0300

    Add project files.

---
+++ ViewModels/Personagens/ListagemPersonagemViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppRpgEtec.Services.Personagens;
+using AppRpgEtec.Models;
+
+namespace AppRpgEtec.ViewModels.Personagens
+{
+    public class ListagemPersonagemViewModel : BaseViewModel
+    {
+        private PersonagemService pService;
+        public ObservableCollection<Personagem> Personagens {  get; set; }
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+        }
+        public ListagemPersonagemViewModel()
+        {
+            string token = Preferences.Get("UsuarioToken", string.Empty);
+            pService = new PersonagemService(token);
+            Personagens = new ObservableCollection<Personagem>();
+            _ = ObterPersonagens();
+        }
+
+        public async Task ObterPersonagens()
+        {
+            try // junto com catch evitara que erros fechem o aplicativo
+            {
+                Personagens = await pService.GetPersonagensAsync();
+                OnPropertyChanged(nameof(Personagens));// Informara a view que houve carregamento
+            }
+            catch (Exception ex)
+            {
+                //Capturara o erro para exibir em tela
+                await Application.Current.MainPage.DisplayAlert("Ops", ex.Message + "Detalhes: " + ex.InnerException, "Ok");
+            }
+        }
+
+        
+    }
+}
